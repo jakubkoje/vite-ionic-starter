cb6598d2ee0f1d9018e0e8c77e2acf92f97ac510
Jakub Jelínek <jakubkoje@gmail.com>
2022-04-21 19:22:16 +0200

    eslint more rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,20 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-    'root': true,
-    'extends': [
-        'plugin:vue/vue3-recommended',
-        'eslint:recommended',
-        '@vue/eslint-config-typescript/recommended'
-    ],
-    'env': {
-        'vue/setup-compiler-macros': true
-    },
-    rules: {
-        quotes: ['error', 'single'],
-        semi: ['error', 'never'],
-        'vue/multi-word-component-names': 'off'
-    }
+  'root': true,
+  'extends': [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended'
+  ],
+  'env': {
+    'vue/setup-compiler-macros': true
+  },
+  rules: {
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'vue/multi-word-component-names': 'off',
+    'no-trailing-spaces': 'error',
+    'indent': ['error', 2]
+  }
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,21 @@
 import {createRouter, createWebHashHistory} from '@ionic/vue-router'
 
 const routes = [
-    // routes go here
-    {
-        path: '/',
-        redirect: '/home'
-    },
-    {
-        path: '/home',
-        name: 'Home',
-        component: () => import('../views/Home.vue')
-    }
+  // routes go here
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
+  }
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
-    routes,
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes,
 })
 
 export default router
